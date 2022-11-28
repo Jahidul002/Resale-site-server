8353cd8848469ec13291c1b5fa386d0008c79619
unknown <emon.me44@gmail.com>
2022-11-28 23:00:16 +0600

    aonothe route added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const { query } = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -15,13 +16,38 @@ function run() {
     try {
         const DataCollention = client.db('assignment12').collection('alldata')
         const CatagoryCollention = client.db('assignment12').collection('catagory')
+        const productCollention = client.db('assignment12').collection('product')
+        const userCollention = client.db('assignment12').collection('user')
 
         app.get('/alldata', async (req, res) => {
             const query = {}
             const data = await DataCollention.find(query).toArray()
             res.send(data)
         })
+        app.delete('/alldata/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const result = await DataCollention.deleteOne(query)
+            res.send(result)
+        })
 
+        app.get('/alldata/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectID(id) }
+            const result = await DataCollention.findOne(query)
+            res.send(result)
+        })
+        app.post('/user', async (req, res) => {
+            const data = req.body
+            const result = await userCollention.insertOne(data)
+            res.send(result)
+        })
+        app.get('/user', async (req, res) => {
+            const query = {}
+            const result = await userCollention.find(query).toArray()
+            res.send(result)
+        })
         app.get('/alldatas', async (req, res) => {
             let query = {}
             if (req.query.catagory) {
@@ -37,6 +63,15 @@ function run() {
             const data = await CatagoryCollention.find(query).toArray()
             res.send(data)
         })
+
+        app.post('/product', async (req, res) => {
+            const data = req.body
+            console.log(data);
+            const result = await DataCollention.insertOne(data)
+            res.send(result)
+        })
+
+
 
 
     }
